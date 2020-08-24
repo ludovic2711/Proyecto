dead1db39f5bd5567b2c051bdfb4e03d692baa5c
ludovic2711 <70164746+ludovic2711@users.noreply.github.com>
2020-08-24 12:42:48 -0500

    Add files via upload

---
+++ objetivos.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Atlas</title>
+	<link rel="stylesheet" type="text/css" href="estilo2.css">
+	<link rel="icon" href="Logo.ico" type="image/png">
+</head>
+<body>
+<table id="banner">
+	<tr>
+	<td width="18.5%" align="center"><a href="inicio.php"><button class="b1">Inicio</button></a></td>
+	<td width="18.5%" align="center"><a><button class="b1">Categorias</button></a></td>
+	<td width="25%" align="center"><img src="Logo.png" id="logo"></td>
+	<td width="18.5%" align="center"><a><button class="b1">Objetivos</button></a></td>
+	<td width="18.5%" align="center"><a><button class="b1">Contactenos</button></a></td>
+    </tr>
+</table>
+<table id="table1" align="center" border="1">
+<tr>
+	<td align="center" id="p1">Objetivo</td>
+</tr>
+<tr>
+	<td align="left" id="p2">Vision:</td>
+
+</tr>
+<tr>
+	<td align="left">asdadasasdadsadasdabdafdyadasdh</td>
+</tr>
+<tr>
+	<td></td>
+</tr>
+<tr>
+	<td></td>
+</tr>
+
+</table>
+</body>
+</html>
